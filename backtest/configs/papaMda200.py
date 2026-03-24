8f0902e828f529f614e7b749dc5eb6f780c98280
Peter Kay <peter@cybercominc.com>
2026-03-23 19:31:06 -1000

    first working version with mda logic

---
+++ backtest/configs/papaMda200.py
@@ -0,0 +1,23 @@
+""" 
+papaOrig.py
+Papa bear original 13 
+"""
+import sys
+from pathlib import Path
+from datetime import date
+# Make backtest/ the root for imports
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))  # parents[1] = backtest/
+from src.momda import run_momda
+
+CONFIG = {
+    "tickers_param": [
+        'VTV', 'VUG', 'VIOV', 'VIOG', 'VEA', 'VWO', 'VNQ',
+        'PDBC', 'IAU', 'EDV', 'VGIT', 'VCLT', 'BNDX'
+    ],
+    "mda_param": 200,
+    "file_suffix_param": "mda200"
+}
+
+
+if __name__ == "__main__":
+    run_momda(**CONFIG)

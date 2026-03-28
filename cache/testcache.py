d80b8f9052a28ca4a7b35acf749d6b1487bd2a1a
Peter Kay <peter@cybercominc.com>
2026-03-28 12:38:52 -1000

    expanded tests

--- cache/testcache.py
+++ cache/testcache.py
@@ -1,10 +1,17 @@
+"""
+testcache.py
+works the yfcache to analyze performance and caching results
 
+next bug: yf downloads are off by a hair, and skip_cache results don't seem to reflect latest yahoo donlaods
+e.g. if skip cache essentially does a refresh, then it should match yahoo which happens only a few seconds later. 
+find out why skip cache doesn't match yahoo
+"""
 import sys
 from pathlib import Path
 import yfinance as yf
 import pandas as pd
 from yfcache import yfcache
-from datetime import date
+from datetime import datetime
 
 # Example script using yfcache class
 cache = yfcache()
@@ -13,32 +20,43 @@ tickers=['AAPL', 'VIOV']
 startd="2019-09-12"
 endd="2019-12-31"
 skip_cache = True
+timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M")
+csvfile = f"/Users/peterkay/Downloads/csv/yfdl{timestamp}.csv"
+
+def same_result (dfa, dfb):
+    return (dfa.final_df.values == dfb.final_df.values).all()
 
 # first pull the cached data
 cache_result = cache.get(tickers, startd, endd) # now the cached version
-df_cached = cache_result.final_df
 
-# then skip cache i.e. download again from YF
+# then skip cache i.e. download again from YF and refresh cache
 skip_result = cache.get(tickers, startd, endd, skip_cache)
-df_skip_cache = skip_result.final_df
+
+after_cache = cache.get(tickers, startd, endd) # now the recently refreshed cached version
 
 # then directly download from YF
 dfy = yf.download(tickers, start=startd, end=endd, auto_adjust=True, progress=False)["Close"] 
+dfy.to_csv(csvfile)
 
 # report the results
+print("*** Cache hits ***")
 print(f"Cached Tickers missed: {cache_result.missed_tickers}. Needed starts: {cache_result.needed_starts}")
 print(f"Skip cache Tickers missed: {skip_result.missed_tickers}. Needed starts: {skip_result.needed_starts}")
+print(f"TEST:0 After cache Tickers missed: {after_cache.missed_tickers}. Needed starts: {after_cache.needed_starts}")
+print("\n\n")
 
+print("*** Comparisons ***")
+print(f"cached matches skip_cache?{same_result(skip_result, cache_result)}")
+print(f"skip_cache matches Yahoo?{(skip_result.final_df.values == dfy.values).all()}")
+print(f"cache matches Yahoo?{(cache_result.final_df.values == dfy.values).all()}")
+print(f"TEST: TRUE after cache matches skip_cache?{same_result(skip_result, after_cache)}")
+print("\n\n")
 
-print(f"cached matches skip_cache?{(df_skip_cache.values == df_cached.values).all()}")
-print(f"cached matches Yahoo?{(df_skip_cache.values == dfy.values).all()}")
-print(f"skip_cache matches Yahoo?{(df_cached.values == dfy.values).all()}")
-
-print(f"Diff skip_cache:dfy {(abs(df_skip_cache - dfy) / df_skip_cache).sum()}")
-print(f"Diff cached:dfy {(abs(df_cached - dfy) / df_cached).sum()}")
-print(f"Diff cached:skip_cache {(abs(df_cached - df_skip_cache) / df_cached).sum()}")
-
-#
-
+print("*** Differences ***")
+print(f"Diff skip_cache:dfy {(abs(skip_result.final_df - dfy) / skip_result.final_df).sum()}")
+print(f"Diff cached:dfy {(abs(cache_result.final_df - dfy) / cache_result.final_df).sum()}")
+print(f"Diff cached:skip_cache {(abs(cache_result.final_df.values - skip_result.final_df.values) / cache_result.final_df.values).sum()}")
+print(f"after cache:skip_cache {(abs(after_cache.final_df.values - skip_result.final_df.values) / after_cache.final_df.values).sum()}")
+print("\n\n")
 
 pass
